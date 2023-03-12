83946b54b0eb8d1fdb4edaf9b3d08dc486e635f0
burak cevizli <burak.cevizli7@gmail.com>
2023-03-13 00:28:01 +0300

    Networking and UI Design

---
+++ Weather/Model/ManagerModel.swift
@@ -0,0 +1,8 @@
+//
+//  ManagerModel.swift
+//  Weather
+//
+//  Created by Adem Burak Cevizli on 12.03.2023.
+//
+
+import Foundation
---
+++ Weather/Model/WeatherManager.swift
@@ -0,0 +1,8 @@
+//
+//  WeatherManager.swift
+//  Weather
+//
+//  Created by Adem Burak Cevizli on 12.03.2023.
+//
+
+import Foundation
